049c97cdfbb9b5574431956aaf1220a6e4d0613e
sognshenglinsong_i <songshenglinsong_i@didiglobal.com>
2022-11-22 22:30:41 +0800

    restTemplate调用高德路线规划接口

--- internal-common/src/main/java/com/ssl/note/constant/AMapConfigConstants.java
+++ internal-common/src/main/java/com/ssl/note/constant/AMapConfigConstants.java
@@ -8,4 +8,14 @@ package com.ssl.note.constant;
 public class AMapConfigConstants {
 
     public static final String DIRECTION_URL = "https://restapi.amap.com/v3/direction/driving";
+    public static final String STATUS = "status";
+
+    public static final String ROUTE = "route";
+
+    public static final String PATHS = "paths";
+
+    public static final String DISTANCE = "distance";
+
+    public static final String DURATION = "duration";
+
 }
--- service-map/src/main/java/com/ssl/note/service/MapDirectionClient.java
+++ service-map/src/main/java/com/ssl/note/service/MapDirectionClient.java
@@ -1,10 +1,19 @@
 package com.ssl.note.service;
 
+import ch.qos.logback.core.status.StatusUtil;
 import com.ssl.note.constant.AMapConfigConstants;
 import com.ssl.note.response.DirectionResponse;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.util.JSONUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 /**
  * @Author: SongShengLin
@@ -18,23 +27,62 @@ public class MapDirectionClient {
     @Value("${amap.key}")
     private String aMapKey;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     /**
      * ?origin=116.481028,39.989643&destination=116.465302,40.004717&extensions=all&output=json&key=f7db1c44b0132b46fb2a78cc0f6e25be
      */
     public DirectionResponse direction(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(AMapConfigConstants.DIRECTION_URL);
-        sb.append("?");
-        sb.append("origin=").append(depLongitude).append(",").append(depLatitude);
-        sb.append("&");
-        sb.append("destination=").append(destLongitude).append(",").append(destLatitude);
-        sb.append("&");
-        sb.append("extensions=all");
-        sb.append("output=json");
-        sb.append("&");
-        sb.append("key=").append(aMapKey);
+        String urlBaseBuilder = AMapConfigConstants.DIRECTION_URL +
+                "?" +
+                "origin=" + depLongitude + "," + depLatitude +
+                "&" +
+                "destination=" + destLongitude + "," + destLatitude +
+                "&" +
+                "extensions=base" +
+                "output=json" +
+                "&" +
+                "key=" + aMapKey;
 
-        System.out.println(sb.toString());
-        return new DirectionResponse();
+        // restTemplate调用高德路线规划的接口
+        ResponseEntity<String> entity = restTemplate.getForEntity(urlBaseBuilder, String.class);
+        System.out.println("body:"+entity.getBody());
+
+        DirectionResponse directionResponse = parseDirectionEntity(entity.getBody());
+
+        return directionResponse;
+    }
+
+    private DirectionResponse parseDirectionEntity(String direction) {
+        DirectionResponse directionResponse = null;
+        try {
+            JSONObject directionJson = JSONObject.fromObject(direction);
+            if (directionJson.has(AMapConfigConstants.STATUS)) {
+                Integer status = directionJson.getInt(AMapConfigConstants.STATUS);
+                if (Objects.equals(status, 1)) {
+                    if (directionJson.has(AMapConfigConstants.ROUTE)) {
+                        JSONObject routeJson = directionJson.getJSONObject(AMapConfigConstants.ROUTE);
+                        if (routeJson.has(AMapConfigConstants.PATHS)) {
+                            JSONArray pathArrayJson = routeJson.getJSONArray(AMapConfigConstants.PATHS);
+                            // 取数组第一个为"速度最快"
+                            JSONObject pathJson = pathArrayJson.getJSONObject(0);
+                            directionResponse = new DirectionResponse();
+                            if (pathJson.has(AMapConfigConstants.DISTANCE)) {
+                                int distance = pathJson.getInt(AMapConfigConstants.DISTANCE);
+                                directionResponse.setDistance(distance);
+                            }
+                            if (pathJson.has(AMapConfigConstants.DURATION)) {
+                                int duration = pathJson.getInt(AMapConfigConstants.DURATION);
+                                directionResponse.setDuration(duration);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return directionResponse;
     }
 }
