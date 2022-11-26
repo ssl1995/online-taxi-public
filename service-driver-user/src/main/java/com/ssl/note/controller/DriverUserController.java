39e6924ebd4382e493659be2cf1cb95cd37c7f50
songshenglin_i <872912679@qq.com>
2022-11-26 16:11:06 +0800

    保存司机信息

--- service-driver-user/src/main/java/com/ssl/note/controller/DriverUserController.java
+++ service-driver-user/src/main/java/com/ssl/note/controller/DriverUserController.java
@@ -1,9 +1,12 @@
 package com.ssl.note.controller;
 
 import com.ssl.note.dto.DriverUser;
+import com.ssl.note.dto.ResponseResult;
 import com.ssl.note.service.DriverUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,9 +20,15 @@ public class DriverUserController {
     @Autowired
     private DriverUserService driverUserService;
 
+    @PostMapping("/user")
+    public ResponseResult<String> saveUser(@RequestBody DriverUser driverUser) {
+        return driverUserService.saveUser(driverUser);
+    }
+
 
     @GetMapping("/test")
     public DriverUser test(Long id) {
         return driverUserService.getTest(id);
     }
+
 }
--- service-driver-user/src/main/java/com/ssl/note/service/DriverUserService.java
+++ service-driver-user/src/main/java/com/ssl/note/service/DriverUserService.java
@@ -1,10 +1,12 @@
 package com.ssl.note.service;
 
 import com.ssl.note.dto.DriverUser;
+import com.ssl.note.dto.ResponseResult;
 import com.ssl.note.mapper.DriverUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -19,12 +21,20 @@ public class DriverUserService {
     @Autowired
     private DriverUserMapper driverUserMapper;
 
-    public DriverUser getTest(Long id){
+    public DriverUser getTest(Long id) {
 
         HashMap<String, Object> map = new HashMap<>();
-        map.put("id",id);
+        map.put("id", id);
         List<DriverUser> driverUsers = driverUserMapper.selectByMap(map);
         return driverUsers.get(0);
+    }
+
+    public ResponseResult<String> saveUser(DriverUser driverUser) {
+        LocalDateTime now = LocalDateTime.now();
+        driverUser.setGmtCreate(now);
+        driverUser.setGmtModified(now);
+        driverUserMapper.insert(driverUser);
+        return ResponseResult.success("");
     }
 
 
